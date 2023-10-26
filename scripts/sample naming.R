0f4e885de0a149057a0e525378386326b04aac9e
cbwall <cbw0047@gmail.com>
2023-10-26 13:44:02 -0700

    remove old file archive

--- scripts/sample naming.R
+++
@@ -1,7 +0,0 @@
-getwd()
-
-samples<-read.csv("flowcytom.csv")
-head(samples)
-samples$names<-interaction(samples$Time, samples$Tank, samples$fluorescence, sep="_")
-
-write.csv(samples, "samples.csv")
